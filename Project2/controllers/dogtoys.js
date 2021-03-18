e679a55532e1db96e97c46cf1c1e910dd559591a
pdbrooks <PDBrooks01@outlook.com>
2021-03-17 20:49:38 -0400

    Seed Route is working

--- Project2/controllers/dogtoys.js
+++ Project2/controllers/dogtoys.js
@@ -15,7 +15,52 @@ router.get('/',(req,res)=>{
   })
 })
 
+
+
+
+
 //Seed Route
+router.get('/seed',(req,res)=>{
+  Dogtoy.create([
+    {
+      name:"Kong® Classic",
+      brand:"Kong",
+      img:"https://9ed48207422fa7fc5013-a6297eb5ec0f30e883355c8680f3b2d6.ssl.cf2.rackcdn.com/T1_1_1000x1000.jpg",
+      size:[0,1,2,3,4,5]
+    },
+    {
+      name:"Kong® Extreme",
+      brand:"Kong",
+      img:"https://9ed48207422fa7fc5013-a6297eb5ec0f30e883355c8680f3b2d6.ssl.cf2.rackcdn.com/K1_1_1000x1000.jpg",
+      size:[0,1,2,3,4,5]
+    },
+    {
+      name:"KONG® Floppy Knot Fox Dog Toy",
+      brand:"Kong",
+      img:"https://s7d2.scene7.com/is/image/PetSmart/5260993?$CLEARjpg$",
+      size:[1,2,3]
+    }
+  ],(err,data) =>{
+    if (err) {
+      console.log(err)
+    }
+    res.redirect('/ratethis')
+  })
+})
+
+
+//POST Route
+router.post('/',(req,res)=>{
+  Dogtoy.create(req.body,(error,createdToy)=>{
+    if (error){
+      console.log(error)
+      res.send(error)
+    }else {
+      // console.log(createdToy)
+      res.redirect('/ratethis')
+    }
+  })
+})
 
 
 
--- Project2/models/dogtoys.js
+++ Project2/models/dogtoys.js
@@ -5,7 +5,7 @@ const dogtoySchema = new Schema({
   name:{type: String, required:true},
   brand:{type: String, required:true},
   img:{type:String,required:true},
-  size:{type: Number, required:true}
+  size:{type: Array, required:true}
 })
 
 const Dogtoy = model('Dogtoy',dogtoySchema)
